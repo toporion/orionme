4e53b443080d87cd64fdf9df093e0a12b539918b
salman <newcolor22@gmail.com>
2025-02-13 00:44:05 +0600

    try

--- server/Index.js
+++ server/Index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { scheduleJobs } = require("./cronJobs");
+
 const app = express();
 const cors = require('cors');
 // const bodyParser=require('body-parser')
